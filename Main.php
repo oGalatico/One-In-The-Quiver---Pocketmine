6c81a1a8724733c605d44315a025525ac4b20338
Emre Yavuzyiğit <emre.yavuz.842@gmail.com>
2016-01-18 20:20:45 +0200

    i am writing.. code

--- Main.php
+++ Main.php
@@ -1,59 +1,62 @@
 <?php
-namespace tw\TurfWars;
+
+// MineDogsTeam @EmreTr1
+
+namespace lucky\emre;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\entity\Effect;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\level\Position;
-use pocketmine\level\Level;
-use pocketmine\item\Item;
-use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\OfflinePlayer;
-use pocketmine\utils\Config;
+use pocketmine\command\CommandExecutor;
 use pocketmine\command\ConsoleCommandSender;
+use pocketmine\utils\Config;
 use pocketmine\math\Vector3;
-use pocketmine\scheduler\PluginTask;
-use pocketmine\scheduler\CallbackTask;
-use pocketmine\block\Block;
-use pocketmine\event\entity\EntityDeathEvent;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\tile\Sign;
-use pocketmine\tile\Tile;
-use pocketmine\utils\TextFormat;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use onebone\economyapi\EconomyAPI;
-use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\event\block\BlockPlaceEvent;
-use pocketmine\event\player\PlayerCommandPreprocessEvent;
-use ResetChest\Main as ResetChest;
-use killrate\Main as KillRate;
-use pocketmine\event\player\PlayerRespawnEvent;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\level\particle\DustParticle;
+use pocketmine\level\sound\ClickSound;
+use pocketmine\level\Position;
+use pocketmine\event\entity\EntityTeleportEvent;
 
-class Main extends PluginBase implements Listener
-{	
-	public $data;
-	
-	private static $object = null;
-	
-	public static function getInstance(){
-		return self::$object;
+use Main\BeginTask;
+
+public $reds = [];
+public $blues = [];
+
+class Main extends PluginBase implements Listener{
+
+    public function OnEnable(){
+        $this->getLogger()->info(TextFormat::GREEN . "TurfWars Plugini Açıldı!!");
 	}
 
-	public function onEnable() {
-		$this->api = EconomyAPI::getInstance();
-		$this->getServer()->getPluginManager()->registerEvents($this,$this);
-		$this->getServer()->getScheduler()->scheduleRepeatingTask(new CallbackTask([$this,"gameTimber"]),20);
-		@mkdir($this->getDataFolder(), 0777, true);
-		$this->points = new Config($this->getDataFolder()."points.yml", Config::YAML);
-		$this->config=new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
-		if($this->config->exists("lastpos"))
-		{
-			$this->sign=$this->config->get("sign");
-			// will continue :)
+    public function OnDisable(){
+        $this->getLogger()->info(TextFormat::RED . "TurfWars Plugini Kapandıı!!");
+	}
+	
+    public function OnJoin(Player, PlayerJoinEvent $event, $player){
+        if($player->isOnline()){
+		$event->getplayer()->sendMessage("§aWelocome to The §bTurfWars §aServer! §eYou Playing On §5***.***.** Server! §6Have Fun :)");
+		$player->getInventory()->addItem(Item::get(Item::CLOCK, 0, 1));
+		}
+	
+public function onRun($tick){
+	foreach($this->plugin->getServer()->getOnlinePlayers() as $p){
+		if($this->plugin->gameduration == 642){
+		$onp = count($this->plugin->getServer()->getOnlinePlayers());
+		$need = 8;
+		$total = $need - $onp;
+		$t2 = $onp - $need;
+         $p->sendTip("Online: ".count($this->plugin->getServer()->getOnlinePlayers())."/8");
+		 $p->sendPopup("".$total. "Need Player!");
+	}		
+}
+//i am writing...
